c63bf9661b64dc355b2ca9768cb9b7cf1caafdab
MichalRabada <22c20@srobarka.sk>
2025-04-23 13:24:28 +0200

    shuffle, toggleLights

--- src/main/java/sk/srobarka/LightsOut/Main.java
+++ src/main/java/sk/srobarka/LightsOut/Main.java
@@ -3,6 +3,7 @@ package sk.srobarka.LightsOut;
 
 
 public class Main {
+
     public static void main(String[] args) {
         }
     }
--- src/main/java/sk/srobarka/LightsOut/core/Field.java
+++ src/main/java/sk/srobarka/LightsOut/core/Field.java
@@ -1,5 +1,7 @@
 package sk.srobarka.LightsOut.core;
 
+import java.util.Random;
+
 public class Field {
 
     private int rowCount;
@@ -13,10 +15,59 @@ public class Field {
 
         generate(difficulty);
     }
+
     private void generate(int moves){
         initField();
         shuffle(moves);
     }
+
+    private void shuffle (int moves)
+    {
+        var random= new Random();
+        for (var move=0; move<moves; move++)
+        {
+            var row=random.nextInt(rowCount);
+            var column=random.nextInt(columnCount);
+            toggleLights(row, column);
+        }
+    }
+
+    public void toggleLights (int row, int column)
+    {
+        this.lights[row][column].toggle();
+        if (row>0)
+        {
+            this.lights[row-1][column].toggle();
+        }
+        if (row<this.rowCount-1)
+        {
+            this.lights[row+1][column].toggle();
+        }
+        if (column>0)
+        {
+            this.lights[row][column-1].toggle();
+        }
+        if (column<this.columnCount-1)
+        {
+            this.lights[row][column+1].toggle();
+        }
+    }
+
+    public int getRowCount ()
+    {
+        return this.rowCount;
+    }
+
+    public int getColumnCount ()
+    {
+        return this.columnCount;
+    }
+
+    public Light getLight (int row, int column)
+    {
+
+    }
+
     private void initField(){
         for(var row=0; row<this.rowCount; row++) {
             for (var column = 0; column < this.columnCount; column++) {
